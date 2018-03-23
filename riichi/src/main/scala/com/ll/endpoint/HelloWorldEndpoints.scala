94a0dea0d084f1b8f06151a517e135e18e73d426
Nikita Panteleev <nikita.panteleev@phystech.edu>
2018-03-24 03:26:06 +0400

    #1 Add WS hello world.

--- riichi/src/main/scala/com/ll/endpoint/HelloWorldEndpoints.scala
+++ riichi/src/main/scala/com/ll/endpoint/HelloWorldEndpoints.scala
@@ -1,13 +1,14 @@
 package com.ll.endpoint
 
+import akka.NotUsed
 import cats.effect.Effect
 import com.ll.utils.Logging
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage, UpgradeToWebSocket}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.stream.javadsl.Flow
+import akka.http.scaladsl.server.{Route, RouteResult}
+import akka.stream.Materializer
 
 class HelloWorldEndpoints[F[_]: Effect] extends Logging {
 
@@ -18,13 +19,40 @@ class HelloWorldEndpoints[F[_]: Effect] extends Logging {
       }
     }
 
-  def endpoints(): Route =
-    helloRoute
+  def greeterWebSocketService(implicit mat: Materializer): Flow[Message, TextMessage, NotUsed] =
+    Flow[Message].mapConcat {
+      case tm: TextMessage => TextMessage(Source.single("Hello ") ++ tm.textStream) :: Nil
+      case bm: BinaryMessage =>
+        bm.dataStream.runWith(Sink.ignore)
+        Nil
+    }
+
+  def wsRoute(implicit mat: Materializer) = path("ws") {
+    get { ctx =>
+      ctx.request match {
+        case req @ HttpRequest(HttpMethods.GET, Uri.Path("/ws"), _, _, _) =>
+          req.header[UpgradeToWebSocket] match {
+            case Some(upgrade) =>
+              log.info("Creating ws connection")
+              val conn = upgrade.handleMessages(greeterWebSocketService)
+              ctx.complete(conn)
+            case None          =>
+              ctx.complete(HttpResponse(400, entity = "Not a valid websocket request!"))
+          }
+        case r: HttpRequest =>
+          r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+          ctx.complete(HttpResponse(404, entity = "Unknown resource!"))
+      }
+    }
+  }
+
+  def endpoints(implicit mat: Materializer): Route =
+    helloRoute ~ wsRoute
 }
 
 object HelloWorldEndpoints {
-  def endpoints[F[_]: Effect](): Route =
-    new HelloWorldEndpoints[F].endpoints()
+  def endpoints[F[_]: Effect]()(implicit mat: Materializer): Route =
+    new HelloWorldEndpoints[F].endpoints(mat)
 }
 
 
