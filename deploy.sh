3f8e192df37776f77b73e59ecf60ad8b0ce8d684
Nikita Panteleev <nikita.panteleev@phystech.edu>
2018-04-29 16:02:46 +0400

    #3 Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Building tavern"
+echo "Pulling latest changes..."
+git pull
+echo "Building jar..."
 sbt assembly
+echo "Building docker..."
 docker build -t tavern:kotone .
 #docker save -o tavern_kotone.tar tavern:kotone
 #scp tavern_kotone.tar root@balmora:/root/tavern/tavern_kotone.tar
 #docker load -i tavern/tavern_kotone.tar
-docker run --net=host tavern:kotone
+echo "Killing the previous docker container and starting new one..."
+docker kill $(docker ps | grep 'tavern.*' | awk '{print $1}')
+docker run -d --net=host tavern:kotone
